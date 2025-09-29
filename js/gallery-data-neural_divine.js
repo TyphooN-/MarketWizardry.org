d1c0dd8ef6144d93f5be65d66225278ee7920e39
TyphooN- <850103+TyphooN-@users.noreply.github.com>
2025-09-29 17:29:23 -0400

    regression fix

--- js/gallery-data-neural_divine.js
+++ js/gallery-data-neural_divine.js
@@ -1,6 +1,6 @@
 // Image paths for neural_divine gallery
 const galleryImagePaths = [
-    "./neural_divine/webp/neural_divine-1930050708-_Dinner_Time__video2-lossy.webp",    "./neural_divine/webp/neural_divine-1929556979-You_transported_me_in_your_universe_video2-lossy.webp",    "./neural_divine/webp/neural_divine-1941161425604595719-Sitting_on_the_couch_video4-lossy.webp",    "./neural_divine/webp/neural_divine-1930050708-_Dinner_Time__video4-lossy.webp"
+    "./neural_divine/webp/neural_divine-1930050708-_Dinner_Time__video4-lossy.webp",    "./neural_divine/webp/neural_divine-1929556979-You_transported_me_in_your_universe_video2-lossy.webp",    "./neural_divine/webp/neural_divine-1941161425604595719-Sitting_on_the_couch_video4-lossy.webp",    "./neural_divine/webp/neural_divine-1930050708-_Dinner_Time__video2-lossy.webp"
 ];
 
 // Initialize gallery when page loads
